ed36a4aaf279cd5f5dd86094184c2e06f1c38fcb
AlanBlanchetNeovision <alan.blanchet@neovision.fr>
2022-07-22 16:36:50 +0200

    [DEV]Added tests for string capitalize
    Added test to further investigate a bug in client installation

--- src/string/capitalize/index.test.ts
+++ src/string/capitalize/index.test.ts
@@ -7,3 +7,36 @@ test('capitalize first word', () => {
 test('capitalize all words of sentence', () => {
   expect('i am good'.capitalize(true)).toBe('I Am Good');
 });
+
+test('capitalize with split', () => {
+  expect(
+    'capitalize with split'
+      .split(' ')
+      .map((d, i) => (i != 0 ? d.capitalize() : d))
+      .join(''),
+  ).toBe('capitalizeWithSplit');
+});
+
+test('empty string', () => {
+  expect(''.capitalize()).toBe('');
+});
+
+test('special char', () => {
+  expect('\\'.capitalize()).toBe('\\');
+});
+
+test('already capitalized', () => {
+  expect('Already'.capitalize()).toBe('Already');
+});
+
+test('Multi spaced capitalize first', () => {
+  expect('i have  a  lot      of     spaces'.capitalize()).toBe(
+    'I have  a  lot      of     spaces',
+  );
+});
+
+test('Multi spaced capitalize all', () => {
+  expect('i have  a  lot      of     spaces'.capitalize(true)).toBe(
+    'I Have  A  Lot      Of     Spaces',
+  );
+});
