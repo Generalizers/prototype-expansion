5d1f2c1d0992f914cd351d8cc1e1aced124d788a
Alan Blanchet <alan@alan-blanchet.fr>
2022-08-26 17:41:22 +0200

    [DEV]Rectangle v2 undefined
    If v2 is undefined but v1 is defined, use v1 for v2.

--- src/rectangle/index.ts
+++ src/rectangle/index.ts
@@ -11,7 +11,7 @@ export class Rectangle {
 
   constructor(items: [[number, number], [number, number]], locked = false) {
     this.v1 = Vector2.from(items[0]);
-    this.v2 = Vector2.from(items[1]);
+    this.v2 = Vector2.from(items[1] ?? [...items[0]]);
     this.lockProportions(locked);
   }
 
