8016e62c5412a4ae0bc01b2bcac978ff5b54f80e
Alan Blanchet <alan@alan-blanchet.fr>
2022-08-28 13:51:04 +0200

    [DEV]Added vector operation on rectangle
    The vector operations were accessible from the rectangle.
    They are now private and we can only access the rectangle instance
    functions

--- src/rectangle/index.ts
+++ src/rectangle/index.ts
@@ -5,13 +5,13 @@ import { Vector2 } from '../vector2';
 type Vector2OrNumberArr = Vector2 | [number, number];
 
 export class Rectangle {
-  private readonly _v1: Vector2;
-  private readonly _v2: Vector2;
+  private readonly v1: Vector2;
+  private readonly v2: Vector2;
   private lockedProportions: boolean = false;
 
   constructor(items: [[number, number], [number, number]], locked = false) {
-    this._v1 = Vector2.from(items[0]);
-    this._v2 = Vector2.from(items[1] ?? [0, 0]);
+    this.v1 = Vector2.from(items[0]);
+    this.v2 = Vector2.from(items[1] ?? [0, 0]);
     this.lockProportions(locked);
   }
 
@@ -39,21 +39,21 @@ export class Rectangle {
   }
 
   static contains(r1: Rectangle, r2: Rectangle) {
-    const [r1x, r1y] = Vector2.seperate([r1._v1, r1._v2]);
-    const [r2x, r2y] = Vector2.seperate([r2._v1, r2._v2]);
+    const [r1x, r1y] = Vector2.seperate([r1.v1, r1.v2]);
+    const [r2x, r2y] = Vector2.seperate([r2.v1, r2.v2]);
     return r1x.isBounded(r2x) && r1y.isBounded(r2y);
   }
 
   get w() {
-    return Math.abs(this._v2.x - this._v1.x);
+    return Math.abs(this.v2.x - this.v1.x);
   }
 
   get h() {
-    return Math.abs(this._v2.y - this._v1.y);
+    return Math.abs(this.v2.y - this.v1.y);
   }
 
   get hypotenuse() {
-    return this._v1.distance(this._v2);
+    return this.v1.distance(this.v2);
   }
 
   get area() {
@@ -66,8 +66,8 @@ export class Rectangle {
 
   get asPosition() {
     return [
-      Math.min(this._v1.x, this._v2.x),
-      Math.min(this._v1.y, this._v2.y),
+      Math.min(this.v1.x, this.v2.x),
+      Math.min(this.v1.y, this.v2.y),
       this.w,
       this.h,
     ];
@@ -87,41 +87,40 @@ export class Rectangle {
   set(n: [number, number, number, number] | [number, number] | number) {
     if (Array.isArray(n)) {
       if (n.length == 4) {
-        this._v1.set([n[0], n[1]]);
-        this._v2.set([n[2], n[3]]);
+        this.v1.set([n[0], n[1]]);
+        this.v2.set([n[2], n[3]]);
       } else if (n.length == 2) {
-        this._v1.set([n[0], n[1]]);
-        this._v2.set([n[0], n[1]]);
+        this.v1.set([n[0], n[1]]);
+        this.v2.set([n[0], n[1]]);
       }
     } else {
-      this._v1.set([n, n]);
-      this._v2.set([n, n]);
+      this.v1.set([n, n]);
+      this.v2.set([n, n]);
     }
     return this;
   }
 
-  set v1(n: [number, number]) {
-    if (this.lockedProportions)
-      this._v2.set(NumberArray.sum([this.w, this.h], n) as [number, number]);
-    this._v1.set(n);
+  sumP1(n: number | number[]) {
+    if (this.lockedProportions) this.v2.sum(n);
+    this.v1.sum(n);
+    return this;
   }
 
-  get v1() {
-    return this._v1 as unknown as [number, number];
+  sumP2(n: number | number[]) {
+    this.v2.sum(n);
+    return this;
   }
 
-  set v2(n: [number, number]) {
-    if (this.lockedProportions)
-      this._v2.set(NumberArray.sum([...this._v1], n) as [number, number]);
-    else this._v2.set(n);
+  get p1() {
+    return [...this.v1] as [number, number];
   }
 
-  get v2() {
-    return this._v2 as unknown as [number, number];
+  get p2() {
+    return [...this.v2] as [number, number];
   }
 
   get x1() {
-    return this._v1.x;
+    return this.v1.x;
   }
 
   get x2() {
@@ -129,7 +128,7 @@ export class Rectangle {
   }
 
   get y1() {
-    return this._v1.y;
+    return this.v1.y;
   }
 
   get y2() {
@@ -138,26 +137,5 @@ export class Rectangle {
 
   lockProportions(locked = true) {
     this.lockedProportions = locked;
-  }
-
-  sum(n: [number, number, number, number] | [number, number] | number) {
-    if (Array.isArray(n)) {
-      if (n.length == 4) {
-        this.set([
-          this.x1 + n[0],
-          this.y1 + n[1],
-          this.x2 + n[2],
-          this.y2 + n[3],
-        ]);
-      } else if (n.length == 2) {
-        this.set([this.x1 + n[0], this.y1 + n[1], this.x2, this.y2]);
-      }
-    } else this.set([this.x1 + n, this.y1 + n, this.x2 + n, this.y2 + n]);
-    return this;
-  }
-
-  minus(n: [number, number, number, number] | [number, number] | number) {
-    if (Array.isArray(n)) return this.sum(n.map((n) => -n) as any);
-    else return this.sum(-n);
   }
 }
--- src/rectangle/lockProportions/index.test.ts
+++ src/rectangle/lockProportions/index.test.ts
@@ -4,6 +4,5 @@ import { Vector2 } from '../../vector2';
 test('lockProportions', () => {
   const rect = Rectangle.from([1, 5], [5, 10]);
   rect.lockProportions();
-  rect.v1 = [2, 6];
-  expect(rect).toStrictEqual(Rectangle.from([2, 6], [6, 11], true));
+  expect(rect.sumP1(1)).toStrictEqual(Rectangle.from([2, 6], [6, 11], true));
 });
